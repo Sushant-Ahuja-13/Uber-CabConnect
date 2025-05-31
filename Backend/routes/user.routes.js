dce63b3947c394d641a1413294e89d6cbc6f1d9c
Sushant Ahuja <12212008@nitkkr.ac.in>
2025-05-31 23:05:34 +0530

    user-login done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const userController = require('../controllers/user.controller.js');
+//Register-Route
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid email'),
     body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
 ], userController.registerUser);
+//Login-Route
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+], userController.loginUser);
 module.exports = router;
