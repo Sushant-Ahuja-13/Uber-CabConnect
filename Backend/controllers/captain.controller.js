1f9083c7438122c86b104cad73fe4cda774a6865
Sushant Ahuja <12212008@nitkkr.ac.in>
2025-06-01 13:43:33 +0530

    comments added

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -2,6 +2,7 @@ const blacklistTokenModel = require('../models/blacklistToken.model.js');
 const captainModel = require('../models/captain.model.js');
 const captainService = require('../services/captain.service.js');
 const { validationResult } = require('express-validator');
+//Controller-Captain-Register
 module.exports.registerCaptain = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -30,6 +31,7 @@ module.exports.registerCaptain = async (req, res, next) => {
     const token = captain.generateAuthToken();
     res.status(201).json({ token, captain });
 }
+//Controller-Captain-Login
 module.exports.loginCaptain = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -48,9 +50,11 @@ module.exports.loginCaptain = async (req, res, next) => {
     res.cookie('token', token);
     res.status(200).json({ token, captain });
 }
+//Controller-Captain-GetProfile
 module.exports.getCaptainProfile = async (req, res, next) => {
     res.status(200).json({ captain: req.captain });
 }
+//Controller-Captain-Logout
 module.exports.logoutCaptain = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization.split(' ')[1];
     await blacklistTokenModel.create({ token });
