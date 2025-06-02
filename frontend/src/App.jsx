c21527d327e5cf9e2c1808170c8a264ba67a0361
Sushant Ahuja <12212008@nitkkr.ac.in>
2025-06-02 12:54:02 +0530

    frontend with form integration backend completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 import UserLogin from "./pages/userLogin";
 import UserSignup from "./pages/UserSignup";
 import CaptainLogin from "./pages/CaptainLogin";
